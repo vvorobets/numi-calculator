73516433f51fab0ddb4dfccff21683df6a958058
vvorobets <vvorobets@gmail.com>
2019-01-02 00:03:14 +0200

    Add some testing env

---
+++ backend/test/functional/note.spec.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const { test, trait } = use('Test/Suite')('Note')
+const Note = use('App/Models/Note')
+
+trait('Test/ApiClient')
+
+test('get list of notes', async ({ client }) => {
+  await Note.create({
+    noteName: 'Adonis 101',
+    noteBody: 'Note content'
+  })
+
+  const response = await client.get('/notes').end()
+
+  response.assertStatus(200)
+  response.assertJSONSubset([{
+    noteName: 'Adonis 101',
+    noteBody: 'Note content'
+  }])
+})
